f4fb08f6beff6ddd3af1bdb52cad4db1936c69b5
mmatheulemos <matheusfelipelemos@hotmail.com>
2022-07-11 10:50:36 -0300

    atual

---
+++ src/SingletonEager.java
@@ -0,0 +1,15 @@
+
+
+public class SingletonEager {
+    
+    private static SingletonEager instancia;
+
+    
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstancia(){
+       return instancia;
+    }
+}
